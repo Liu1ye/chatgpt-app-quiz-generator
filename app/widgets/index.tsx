c2acd1a4b72e116990d5a0ea11f14e1c7757e94f
liu tt <liulytian2406@gmail.com>
2025-11-29 10:54:32 +0800

    add usemounted

--- app/widgets/index.tsx
+++ app/widgets/index.tsx
@@ -3,6 +3,8 @@
 import { useWidgetProps } from '@/app/hooks'
 import Quiz from './quiz'
 import QuizList from './quiz-list'
+import useMounted from '../hooks/use-mount'
+import { Skeleton } from '../components/Skeleton'
 
 const WIDGETS: Record<string, React.ComponentType> = {
   quiz: Quiz,
@@ -13,13 +15,14 @@ const DEFAULT_WIDGET = 'quiz'
 
 const WidgetSelector = () => {
   const widgetProps = useWidgetProps<{ type?: string }>()
+  const isMounted = useMounted()
 
   const widgetType = widgetProps?.type || DEFAULT_WIDGET
 
   const WidgetComponent = WIDGETS[widgetType]
 
-  if (!WidgetComponent) {
-    return <></>
+  if (!WidgetComponent || !isMounted) {
+    return <Skeleton />
   }
 
   return <WidgetComponent />
